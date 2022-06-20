837d69d634e72d6fc54c27614e0f66beca6dbadd
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-20 00:15:37 -0300

    About Page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Container, Title, Subtitle, Hero, Developers } from "./styles";
+import aboutIllustration from "assets/img/_about-illustration.svg";
+import DevCard from 'components/DevCard';
+
+let developers = [
+  {
+    name: "Italo Bruno",
+    description: "Desenvolvedor Front-end, aparesentação de projeto",
+    avatar: "https://github.com/italocabral.png",
+    github: "https://github.com/italocabral",
+    linkedin: "https://www.linkedin.com/in/bruno-italo-cabral/",
+    email: "g1brunoyokima@hotmail.com"
+  },
+  {
+    name: "Emanuel Azevedo",
+    description: "Desenvolvedor Front-end, aparesentação de projeto",
+    avatar: "https://github.com/nashEm8.png",
+    github: "https://github.com/nashEm8",
+    linkedin: "https://www.linkedin.com/in/emanuel-azevedo-7668b91b7/",
+    email: "emanuel82016@outlook.com"
+  },
+  {
+    name: "Edison Júnior",
+    description: "Desenvolvedor Front-end, aparesentação de projeto",
+    avatar: "https://github.com/EdisonJuniorDesigner.png",
+    github: "https://github.com/EdisonJuniorDesigner",
+    linkedin: "https://www.linkedin.com/in/edison-junior/",
+    email: "mage1232010@gmail.com"
+  }
+];
 
 export const About = () => {
+
+  const [developersList, setDevelopersList] = useState(developers);
+
   return (
-    <div>Sobre nós</div>
+    <Container>
+
+      <Hero>
+        <div className="about-text">
+          <Title>Sobre o projeto</Title>
+          <p className=''>
+            Desenvolvido por um grupo de desenvolvedores, com paixão pela praticidade e informação, o HashTag Finder é uma aplicação com o intuito de reunir os trending topics que você desejar, através de uma busca rápida e fácil.<br/> Busque por hashtags, palavras-chave, ou termos mais específicos, e veja o que está acontecendo no momento em tempo real.
+          </p>
+        </div>
+        <div className="about-image">
+          <img src={aboutIllustration} alt=""/>
+        </div>
+      </Hero>
+
+      <Developers>
+        <Subtitle>Quem somos</Subtitle>
+        
+        <div className="card-grid">
+          {
+            developersList.map((developer, index) => (
+              <DevCard key={index} {...developer} />
+            ))
+          }
+
+        </div>
+      </Developers>
+
+    </Container>
   )
 }
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,1 +1,64 @@
-export {}
+import styled from "styled-components";
+
+import heroBackground from "assets/img/about-hero-bg.jpg";
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  color: #fff;
+  background-image: linear-gradient(to bottom, transparent 0%, #0a1744 95%),
+      url(${heroBackground});
+  background-repeat: no-repeat;
+  background-position: 0 -30%;
+  background-size: cover;
+  backdrop-filter: blur(30px);
+  padding: 100px 50px;
+`;
+
+export const Title = styled.h2`
+  font-size: 80px;
+  font-weight: bold;
+  font-family: "Rubik";
+  font-weight: 700;
+  margin: 40px 0;
+
+`;
+
+export const Subtitle = styled.h2`
+  font-size: 54px;
+  font-weight: bold;
+`;
+
+export const Hero = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  padding: 0 30px;
+
+  .about-text{
+    width: 45%;
+    p{
+      font-weight: normal;
+      font-size: 20px;
+    }
+  }
+
+  .about-image {
+    width: 45%;
+    img {
+      width: 640px;
+      height: 540px;
+    }
+  }
+`;
+
+export const Developers = styled.div`
+    width: 70%;
+    margin: 0 auto;
+  .card-grid{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    justify-items: center;
+    margin: 40px auto;
+  }
+`;
