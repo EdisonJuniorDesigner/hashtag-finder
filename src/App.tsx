47173547a59a05e549628ce0f12f2a4a7289e578
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-20 00:14:31 -0300

    Changing Components to Router Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { Router } from "router";
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
 
 function App() {
     return (
         <>
-            <Navbar />
             <Router />
-            <Footer />
         </>
     );
 }
