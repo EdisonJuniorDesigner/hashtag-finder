3441893adbef1876400e0e2acc0eb49dc45cff32
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-21 00:35:30 -0300

    Dynamic Links

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,22 +1,42 @@
+import { useEffect, useState } from "react";
 import { Container } from "./styles";
 import Logo from "../../assets/img/logo.svg";
 import IconInfo from "../../assets/img/icon-info-circle.svg";
+import IconHome from "../../assets/img/icon-home.svg";
 import IconLogin from "../../assets/img/icon-user-alt.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Navbar = () => {
+
+    const [path, setPath] = useState('');
+    const location = useLocation();
+
+    useEffect(() => {
+        setPath(location.pathname);
+    },[location])
+
     return (
         <Container>
             <div className="navbar-logo">
                 <img src={Logo} alt="logo hashtagfinder" />
             </div>
             <div className="navbar-button">
-                <Link to="/about" >
+                {
+                    path === '/' ?
+                    <Link to="/about" >
+                        <button className="navbar-btn-about">
+                            <img src={IconInfo} alt="" />
+                            sobre
+                        </button>
+                    </Link>
+                    :
+                    <Link to="/" >
                     <button className="navbar-btn-about">
-                        <img src={IconInfo} alt="" />
+                        <img src={IconHome} alt="" />
                         sobre
                     </button>
                 </Link>
+                }
                 <button className="navbar-btn-login">
                     <img src={IconLogin} alt="" />
                     login
