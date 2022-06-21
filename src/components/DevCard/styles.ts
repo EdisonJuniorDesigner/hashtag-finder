4868950d9e509ddd5e2f336af07d3ed7dc6258a9
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-21 00:35:16 -0300

    CSS Fixes

--- src/components/DevCard/styles.ts
+++ src/components/DevCard/styles.ts
@@ -10,7 +10,7 @@ export const Container = styled.div`
   background-color: #0B1A49;
   border: 1px solid rgba(255, 255, 255, 0.1);
   border-radius: 10px;
-  padding: 36px 40px;
+  padding: 36px 24px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -18,14 +18,14 @@ export const Container = styled.div`
 `;
 
 export const CardTitle = styled.h3`
-  font-size: 24px;
+  font-size: 21px;
   font-weight: bold;
   color: #72EFDB;
 `;
 
 export const ImageWrapper = styled.div`
-  height: 128px;
-  width: 128px;
+  height: 100px;
+  width: 100px;
   img{
     width: 100%;
     height: 100%;
@@ -37,7 +37,7 @@ export const CardDescription = styled.div`
   width : 100%;
 
   p{
-    font-size: 18px;
+    font-size: 16px;
     line-height: 25px;
   }
 
