fd024483877435b7f71b25dc2fef616e9997cdb7
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-21 17:45:07 -0300

    Responsividade

--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -12,42 +12,70 @@ export const Container = styled.div`
   background-position: 0 -30%;
   background-size: cover;
   backdrop-filter: blur(30px);
-  padding: 100px 50px;
+  padding: 24px 36px;
 `;
 
 export const Title = styled.h2`
-  font-size: 80px;
+  font-size: 64px;
   font-weight: bold;
   font-family: "Rubik";
   font-weight: 700;
-  margin: 40px 0;
+  margin: 0 0 40px 0;
+
+  @media (max-width: 1280px) {
+    font-size: 48px;
+  }
+
+  @media (max-width: 768px) {}
 
 `;
 
 export const Subtitle = styled.h2`
   font-size: 54px;
   font-weight: bold;
+
+  @media (max-width: 768px) {
+    font-size: 36px;
+    margin-bottom: -20px;
+  }
 `;
 
 export const Hero = styled.div`
   display: flex;
-  align-items: flex-start;
   justify-content: space-evenly;
-  padding: 0 30px;
+  padding: 64px 0;
+  height: calc(100vh - 70px);
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 80px 0;
+
+    .about-text, .about-image {
+      width: 100% !important;
+      margin: 24px 0;
+    }
+  }
 
   .about-text{
     width: 45%;
+
     p{
-      font-weight: normal;
-      font-size: 20px;
+      font-weight: 100;
+      font-size: 18px;
+      line-height: 32px;
+      text-align: justify;
     }
   }
 
   .about-image {
     width: 45%;
+    height: 80%;
+    margin-top: 40px;
     img {
-      width: 640px;
-      height: 540px;
+      width: 100%;
+      height: 100%;
     }
   }
 `;
@@ -55,10 +83,17 @@ export const Hero = styled.div`
 export const Developers = styled.div`
     width: 70%;
     margin: 0 auto;
-  .card-grid{
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    justify-items: center;
-    margin: 40px auto;
-  }
+    .card-grid{
+      display: grid;
+      gap: 20px;
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      justify-items: center;
+      margin: 40px auto;
+    }
+
+    @media (max-width: 768px) {
+      width: 100%;
+      margin-top: 120px;
+    }
+
 `;
