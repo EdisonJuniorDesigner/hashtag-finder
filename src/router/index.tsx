1e49a958758ead828e98eafdbf3caba85fce6d15
ItaloCabral <g1brunoyokima@hotmail.com>
2022-08-07 18:24:27 -0300

    Feat(Router): Protected Route verification

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,20 @@
 import { Footer } from "components/Footer";
 import { Navbar } from "components/Navbar";
+import { AuthContext } from "contexts/AuthContext";
 import { About, Home, Login, Dashboard } from "pages";
+import { useContext } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }: any) => {
+    const { authenticated, loading } = useContext(AuthContext);
+
+    if (loading) return <h2>Carregando...</h2>;
+
+    if (!authenticated)  return <Navigate to="/" />;
+
+    return children;
+
+}
 
 export const Router = () => (
     <BrowserRouter>
@@ -10,7 +23,11 @@ export const Router = () => (
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard" element={
+                <ProtectedRoute>
+                    <Dashboard />
+                </ProtectedRoute>}
+            />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
